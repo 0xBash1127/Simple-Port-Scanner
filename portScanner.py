5cd2f74ba0247f6724731c3a28509cc95f6860b0
0xBash1127 <sjohnson1198@outlook.com>
2024-10-20 21:46:19 -0400

    Update portScanner.py
    implemented concurrent futures to scan multiple ports at once

--- portScanner.py
+++ portScanner.py
@@ -1,6 +1,6 @@
 import sys
 import socket
-
+import concurrent.futures
 
 
 
@@ -8,53 +8,76 @@ import socket
 def port_scanner(ip,port):
 
     if "-" in port:
+
         range_of_ports(ip,port)
     
     elif "," in port:
+        
         multiple_ports(ip,port)
     
     else:
-        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    
-            if s.connect_ex((ip,port)) == 0:
-                print(f"Port {port} is open")
-            
-            else:
-                print(f"Port {port} is closed")
 
+        single_port(ip,port)
+        
+        
+def single_port(ip,port):
+     
+    try:
+            
+            print("Scanning...")
+
+            with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        
+                if s.connect_ex((ip,int(port))) == 0:
+
+                    print(f"Port {port} is open")
+                
+                else:
+
+                    print(f"Port {port} is closed")
+
+
+    except socket.error as e:
+        
+        print(f"Error connecting to {port}:{ip} - {e}")
 
 
 def range_of_ports(ip,port):
+
+    print(f"Scanning ports {port}")
+
+    port_split = port.split("-")
+
+    ports = range(int(port_split[0]), int(port_split[1])+ 1 )
     
-    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
 
-        print("Scanning...")
-        ports = port.split("-")
-        for port in range(int(ports[0]), int(ports[1])+ 1 ):
-            if s.connect_ex ((ip,port))== 0:
-                print(f"Port {port} is open")
+        for port in ports:
             
-            else:
-                print(f"Port {port} is closed")
-                        
+            executor.submit(single_port,ip ,port)
 
+        
+    
 
 
 def multiple_ports(ip,port):
+    
+    print(f"Scanning ports {port}")
 
-    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    port_split = port.split("-")
 
-        ports = port.split(",")
-        for port in range(int(ports[0]), int(ports[1])+ 1 ):
-            if s.connect_ex ((ip,port))== 0:
-                print(f"Port {port} is open")
-            
-            else:
-                print(f"Port {port} is closed")
-                
+    ports = range(int(port_split[0]), int(port_split[1])+ 1 )
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
 
+        for port in ports:
+
+            executor.submit(single_port,ip ,port)
+        
+    
 
 if __name__ == "__main__":
+
     ip = sys.argv[1]
 
     port = sys.argv[2]
